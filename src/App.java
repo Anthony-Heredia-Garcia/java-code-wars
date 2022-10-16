0ef8aa1e25d7eff4ea5b4a53295f87f97bda5994
Anthony-Heredia-Garcia <99502244+Anthony-Heredia-Garcia@users.noreply.github.com>
2022-10-16 16:47:02 -0400

    second commit

--- src/App.java
+++ src/App.java
@@ -1,5 +1,17 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println(zeroFuel(100, 50, 1));
+    }
+
+    static String[] stringToArray(String s){
+        return s.split("\\s");
+    }
+
+    static int Liters (double time) {
+        return (int)Math.floor(time * .5);
+    }
+
+    static boolean zeroFuel(double distanceToPump, double mpg, double fuelLeft){
+        return (boolean)((distanceToPump/mpg) <= fuelLeft);
     }
 }
